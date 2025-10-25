0b5363eac31a8a816510060a2e610d05d3dedff6
ऋतिक राज गुप्ता <142139641+RITIKRAJGUPTA@users.noreply.github.com>
2025-10-25 14:39:34 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // change if needed
+  baseURL: "https://studentteacherassignmentbackend.onrender.com/api", // change if needed
 });
 
 // ✅ Automatically add token for protected routes
